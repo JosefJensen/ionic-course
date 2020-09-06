7e530ab701ce335aff6a6992a95f040d6d17614c
JosefJensen <31988041+JosefJensen@users.noreply.github.com>
2020-09-06 04:00:08 +0200

    Fix

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const reasonInput = document.querySelector('#input-reason');
+const amountInput = document.querySelector('#input-reason');
+const cancelBtn = documnet.querySelector('#btn-cancel');
+const confirmBtn = documnet.querySelector('#btn-confirm');
+
+confirmBtn.addEventListener('click', () => {
+    console.log('It Works!');
+})
